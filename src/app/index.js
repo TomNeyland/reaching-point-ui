cdb02b0fa65b07c790cad961e9e68c987afa947f
Adam Karbiener <adam.karbiener@rbm.tv>
2015-06-09 10:50:12 -0400

    Changed the name from guinea pig to Dashboard page

--- src/app/index.js
+++ src/app/index.js
@@ -8,18 +8,10 @@ angular.module('baseangular', ['ngAnimate', 'ngCookies', 'ngSanitize', 'restangu
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       })
-      .state('home.test1', {
-        url: 'home/test1',
-        templateUrl: 'app/main/block.html'
-      })
-      .state('home.test2', {
-        url: 'home/test2',
-        templateUrl: 'app/main/circle.html'
-      })
-      .state('Guinea Pig', {
-        url: '/GP',
-        templateUrl: 'app/GP/GP.html',
-        controller: 'GPCtrl'
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/dash/dash.html',
+        controller: 'DashCtrl'
       })
       .state('Hamster', {
         url: '/Hamster',
