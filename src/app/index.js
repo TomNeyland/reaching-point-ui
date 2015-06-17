ad923a4f01b42894a46225c93c3786358dcd6d23
Cassie Wilcox <cassiewilcox@Cassies-MacBook-Pro.local>
2015-06-16 22:57:10 -0400

    Added Angular UI Bootstrap

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('baseangular', ['ngAnimate', 'ngCookies', 'ngSanitize', 'restangular', 'ui.router'])
+angular.module('baseangular', ['ngAnimate', 'ngCookies', 'ngSanitize', 'restangular', 'ui.router', 'ui.bootstrap'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('login', {
