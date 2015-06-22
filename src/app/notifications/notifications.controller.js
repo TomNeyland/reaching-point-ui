e0fdf79d86d2bb13e5342c956e0b538a4c51a901
Adam Karbiener <adam.karbiener@rbm.tv>
2015-06-22 16:29:13 -0400

    Have the ability to select a message and have it display on the right, or display the 'nothing selected' message if pertinent

--- src/app/notifications/notifications.controller.js
+++ src/app/notifications/notifications.controller.js
@@ -7,33 +7,33 @@ angular.module('baseangular')
 	$scope.notifications = [
 		{
 			subject: '1 So and So Joined Your Campaign!',
-			summary: '1 Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+			summary: 'Some people totally just joined your campaign',
 			body: 'Content 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta dolor quis fugiat sed reiciendis saepe consequuntur consectetur rem itaque est, facilis ipsa obcaecati necessitatibus blanditiis praesentium nisi, maxime perspiciatis, quod.',
 			created: moment(new Date).format('l'),
 			status: 'unread',
 			type: 'campaign',
-			from: 'Reaching Point',
-			to: 'Company Name'
+			from: 'Point of reach',
+			to: 'Jim bob'
 		},
 		{
-			subject: '2 So and So Joined Your Campaign!',
-			summary: '2 Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+			subject: 'You now have 456 influencers',
+			summary: 'You just acquired a lot of more many lots of people influencing for you.',
 			body: 'Content 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta dolor quis fugiat sed reiciendis saepe consequuntur consectetur rem itaque est, facilis ipsa obcaecati necessitatibus blanditiis praesentium nisi, maxime perspiciatis, quod.',
 			created: moment(new Date).format('l'),
 			status: 'deleted',
 			type: 'campaign',
 			from: 'Reaching Point',
-			to: 'Company Name'
+			to: 'Cassie Wilcox'
 		},
 		{
-			subject: '3 So and So Joined Your Campaign!',
+			subject: 'You now have 3 brands working with you',
 			summary: '3 Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
 			body: '3 Content 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta dolor quis fugiat sed reiciendis saepe consequuntur consectetur rem itaque est, facilis ipsa obcaecati necessitatibus blanditiis praesentium nisi, maxime perspiciatis, quod.',
 			created: moment(new Date).format('l'),
 			status: 'unread',
 			type: 'campaign',
 			from: 'Reaching Point',
-			to: 'Company Name'
+			to: 'Adam Karbiener'
 		}
 	];
 
@@ -43,9 +43,14 @@ angular.module('baseangular')
 		$scope.tab = newTab;
 	};
 
-	$scope.setMessage = function(msg) {
-		$scope.selected = msg;
+	$scope.tabIsSet = function(tab) {
+		return $scope.tab === tab;
 	}
+
+	$scope.selMsg = function(msg) {
+			$scope.selectedMsg = msg;
+		}
+
 
 
 });
