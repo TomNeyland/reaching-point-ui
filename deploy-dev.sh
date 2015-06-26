dd77cdfa8b734a137eb97a1177209dbd4b03a3c7
Ian Grail <ian@rbm.tv>
2015-06-26 11:53:37 -0400

    Fixing deploy script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -6,18 +6,28 @@ echo "REV_NAME:" $REV_NAME
 # Create tarball
 tar -C dist -cvf artifacts/$REV_NAME.tar .
 
+# Create /var/www directory if not exists
+ssh root@dev.reachingpoint.com "mkdir -p /var/www/$REV_NAME;"
+
 # Transfer tarball
-scp artifacts/$REV_NAME.tar root@dev.reachingpoint.com:/var/www
+scp artifacts/$REV_NAME.tar root@dev.reachingpoint.com:/var/www/$REV_NAME.tar
+if [ $? -ne 0 ]; then
+  echo "Tarball Transfer Failed"
+  exit $?
+fi
 
 # Transfer tarball
 # Backup current package
 # Position new package to be served
 # Remove the backup
 ssh root@dev.reachingpoint.com "
+  mkdir -p /var/www/$REV_NAME;
+  mkdir -p /var/www/$CIRCLE_PROJECT_REPONAME;
   tar -xf /var/www/$REV_NAME.tar -C /var/www/$REV_NAME;
-  mv /var/www/$CIRCLE_PROJECT_REPONAME /var/www/$CIRCLE_PROJECT_REPONAME_backup
-  mv /var/www/$REV_NAME /var/www/$CIRCLE_PROJECT_REPONAME
-  rm -rf /var/www/$CIRCLE_PROJECT_REPONAME_backup
+  mv /var/www/$CIRCLE_PROJECT_REPONAME /var/www/${CIRCLE_PROJECT_REPONAME}_backup;
+  mv /var/www/$REV_NAME /var/www/$CIRCLE_PROJECT_REPONAME;
+  rm -rf /var/www/${CIRCLE_PROJECT_REPONAME}_backup;
 "
-echo $
-exit $
+
+echo "Unpacking Tarball Result: " $?
+exit $?
